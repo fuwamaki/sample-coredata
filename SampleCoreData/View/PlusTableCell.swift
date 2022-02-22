251cf409c0d0897772cdf940f39fc6b66cf53055
fuwamaki <variety.fuwa.3@gmail.com>
2022-02-22 14:02:31 +0900

    add PlusTableCell

---
+++ SampleCoreData/View/PlusTableCell.swift
@@ -0,0 +1,18 @@
+//
+//  PlusTableCell.swift
+//  SampleCoreData
+//
+//  Created by fuwamaki on 2022/02/22.
+//
+
+import UIKit
+
+final class PlusTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
+
+// MARK: NibLoadable
+extension PlusTableCell: NibLoadable {}
--- SampleCoreData/ViewController/FruitListViewController.swift
+++ SampleCoreData/ViewController/FruitListViewController.swift
@@ -14,6 +14,7 @@ final class FruitListViewController: UIViewController {
             tableView.delegate = self
             tableView.dataSource = self
             tableView.registerForCell(FruitListTableCell.self)
+            tableView.registerForCell(PlusTableCell.self)
         }
     }
 
@@ -29,16 +30,24 @@ final class FruitListViewController: UIViewController {
 // MARK: UITableViewDataSource
 extension FruitListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
+        return list.count + 1
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: FruitListTableCell.defaultReuseIdentifier,
-            for: indexPath
-        ) as! FruitListTableCell
-        cell.render(list[indexPath.row])
-        return cell
+        switch indexPath.row {
+        case list.count:
+            return tableView.dequeueReusableCell(
+                withIdentifier: PlusTableCell.defaultReuseIdentifier,
+                for: indexPath
+            ) as! PlusTableCell
+        default:
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: FruitListTableCell.defaultReuseIdentifier,
+                for: indexPath
+            ) as! FruitListTableCell
+            cell.render(list[indexPath.row])
+            return cell
+        }
     }
 }
 
@@ -46,5 +55,29 @@ extension FruitListViewController: UITableViewDataSource {
 extension FruitListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        switch indexPath.row {
+        case list.count:
+            let alert = UIAlertController(title: "Add", message: nil, preferredStyle: .alert)
+            alert.addTextField { textField in
+                textField.delegate = self
+            }
+            alert.addAction(UIAlertAction(title: "Create", style: .default, handler: { _ in
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            present(alert, animated: true)
+        default:
+            let alert = UIAlertController(title: "Edit", message: nil, preferredStyle: .alert)
+            alert.addTextField { textField in
+                textField.delegate = self
+                textField.text = self.list[indexPath.row].name
+            }
+            alert.addAction(UIAlertAction(title: "Update", style: .default, handler: { _ in
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            present(alert, animated: true)
+        }
     }
 }
+
+// MARK: UITextFieldDelegate
+extension FruitListViewController: UITextFieldDelegate {}
